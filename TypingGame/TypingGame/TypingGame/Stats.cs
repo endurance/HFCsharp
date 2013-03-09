e1acf5faea33b3cc9e28589021f9b0d356d4d683
Endurance Idehen <jeidehen@gmail.com>
2013-03-09 13:26:47 -0600

    Typing Game
    Playing with the timer in C# .NET.
    The letter will not remove itself when a key is pressed in the listbox

---
+++ TypingGame/TypingGame/TypingGame/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypingGame
+{
+    class Stats
+    {
+        public int Total = 0;
+        public int Missed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void Update(bool CorrectKey)
+        {
+            Total++;
+            if (!CorrectKey)
+            {
+                Missed++;
+            }
+            else
+            {
+                Correct++;
+            }
+            Accuracy = 100 * Correct / (Missed + Correct);
+        }
+    }
+}
