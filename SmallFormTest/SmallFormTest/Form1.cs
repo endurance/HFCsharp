cb14389ccf87579a5bd3e69c434f642fef935552
Endurance Idehen <jeidehen@gmail.com>
2013-03-08 22:44:23 -0600

    Test of event handling
    Change label color depending on if a checkbox is check

---
+++ SmallFormTest/SmallFormTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SmallFormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label1.BackColor = Color.Red;
+            }
+            else
+            {
+                MessageBox.Show("The change color checkbox is not checked.");
+            }
+        }
+
+    }
+}
